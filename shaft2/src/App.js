a854bb1ce6d9c4a5ac001350fda88e9e07f69ab9
Robbity <robbielaughlen@gmail.com>
2024-07-24 00:10:10 -0700

    my face

---
+++ shaft2/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import balderImage from './img/even more fucked.jpg';
+
+function App() {
+  return (
+    <div className="App">
+      <img src={balderImage} alt="Balder" className="full-screen-image" />
+    </div>
+  );
+}
+
+export default App;
