fc0e34e5fe02236ccdd6b0c3415a89fdc0d3fbbd
Robbity <robbielaughlen@gmail.com>
2025-01-29 00:52:42 -0800

    that 3d swag #ualreadykno

--- shaft2/src/App.js
+++ shaft2/src/App.js
@@ -1,13 +1,42 @@
 import './App.css';
 import balderImage from './img/even more fucked.jpg';
+import shaftcity from './img/shaftcitytemp.png';
+import { useState, useEffect } from 'react';
 
 function App() {
+  const [mousePosition, setMousePosition] = useState({ x: 0, y: 0 });
+  
+  useEffect(() => {
+    const handleMouseMove = (e) => {
+      // Calculate mouse position as percentage of window
+      const x = (e.clientX / window.innerWidth) * 100;
+      const y = (e.clientY / window.innerHeight) * 100;
+      setMousePosition({ x, y });
+    };
+
+    window.addEventListener('mousemove', handleMouseMove);
+    
+    return () => {
+      window.removeEventListener('mousemove', handleMouseMove);
+    };
+  }, []);
+
   return (
     <div className="App">
       <div className="text-center">
-        <img src={balderImage} alt="Balder" className="full-screen-image" />
-        <div className='title'>SHAFTCITY</div>
-        <div className='date'>MARCH 15TH</div>
+        <div className="image-container">
+          <img 
+            src={shaftcity} 
+            alt="Balder" 
+            className="full-screen-image"
+            style={{
+              transform: `translate(${(mousePosition.x - 50) * -0.5}px, ${(mousePosition.y - 50) * -0.5}px)`,
+              transition: 'transform 0.1s ease-out'
+            }}
+          />
+        </div>
+        <a href="https://open.spotify.com/artist/5GpFs0JmuMFCGrIMyqO1JI?si=aAM0t-RJSWeD6rp_JKlEOg" className='title-link' target="_blank">SHAFTCITY</a>
+        <div className='date'>LIVE - MARCH 15TH</div>
       </div>
     </div>
   );
