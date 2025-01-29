4c754a8c7bf1e2b4580a712c257cc36259df73d5
Robbity <robbielaughlen@gmail.com>
2025-01-29 12:24:38 -0800

    adjusted to use parallaxjs

--- shaft2/src/App.js
+++ shaft2/src/App.js
@@ -1,41 +1,46 @@
 import './App.css';
 import balderImage from './img/even more fucked.jpg';
 import shaftcity from './img/shaftcitytemp.png';
-import { useState, useEffect } from 'react';
+import { useEffect, useRef } from 'react';
+import Parallax from 'parallax-js';
 
 function App() {
-  const [mousePosition, setMousePosition] = useState({ x: 0, y: 0 });
-  
-  useEffect(() => {
-    const handleMouseMove = (e) => {
-      // Calculate mouse position as percentage of window
-      const x = (e.clientX / window.innerWidth) * 100;
-      const y = (e.clientY / window.innerHeight) * 100;
-      setMousePosition({ x, y });
-    };
+  const sceneRef = useRef(null);
 
-    window.addEventListener('mousemove', handleMouseMove);
-    
-    return () => {
-      window.removeEventListener('mousemove', handleMouseMove);
-    };
+  useEffect(() => {
+    const scene = sceneRef.current;
+    if (scene) {
+      const parallaxInstance = new Parallax(scene, {
+        relativeInput: true,
+        clipRelativeInput: false,
+        hoverOnly: false,
+        scalar: 15 // Adjust this value to control movement intensity
+      });
+
+      // Cleanup on unmount
+      return () => parallaxInstance.destroy();
+    }
   }, []);
 
   return (
     <div className="App">
       <div className="text-center">
         <div className="image-container">
-          <img 
-            src={shaftcity} 
-            alt="Balder" 
-            className="full-screen-image"
-            style={{
-              transform: `translate(${(mousePosition.x - 50) * -0.5}px, ${(mousePosition.y - 50) * -0.5}px)`,
-              transition: 'transform 0.1s ease-out'
-            }}
-          />
+          <div id="scene" ref={sceneRef}>
+            <div data-depth="0.5" className="layer">
+              <img 
+                src={shaftcity} 
+                alt="Balder" 
+                className="full-screen-image"
+              />
+            </div>
+          </div>
         </div>
-        <a href="https://open.spotify.com/artist/5GpFs0JmuMFCGrIMyqO1JI?si=aAM0t-RJSWeD6rp_JKlEOg" className='title-link' target="_blank">SHAFTCITY</a>
+        <a href="https://open.spotify.com/artist/5GpFs0JmuMFCGrIMyqO1JI?si=aAM0t-RJSWeD6rp_JKlEOg" 
+           className='title-link' 
+           target="_blank">
+          SHAFTCITY
+        </a>
         <div className='date'>LIVE - MARCH 15TH</div>
       </div>
     </div>
